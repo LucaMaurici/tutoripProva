2a1d82d9741b89cefe12e3d26bd2149131ad68a5
Lorenzo Mattia <lomattia2011@gmail.com>
2020-04-26 12:08:20 +0200

    Aggiunti metodi save update delete

--- tutoripProva/tutoripProva/REST/RestService.cs
+++ tutoripProva/tutoripProva/REST/RestService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using tutoripProva.Models;
@@ -35,6 +36,88 @@ namespace tutoripProva.REST
                 Debug.WriteLine("\tERROR {0}", ex.Message);
             }
             return utenti;
+        }
+
+        public async Task SaveElements(Utente user, String uri, bool isNewItem = false)
+        {
+            var json = JsonConvert.SerializeObject(user);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = null;
+            if (isNewItem) //??
+            {
+                response = await _client.PostAsync(uri, content);
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine(@"\tTodoItem successfully saved."); //Non utile
+            }
+        }
+
+        public async Task UpdateElements(Utente user, String uri, bool isNewItem = false)
+        {
+            var json = JsonConvert.SerializeObject(user);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+            if (isNewItem) //??
+            {
+                response = await _client.PutAsync(uri, content);
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine(@"\tTodoItem successfully saved."); //Non utile
+            }
+        }
+
+        //versione update e save tutta in uno
+        public async Task SaveOrUpdateElements(Utente user, String uri, bool isNewItem = false)
+        {   
+            try
+            {
+                var json = JsonConvert.SerializeObject(user);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                if (isNewItem)
+                {
+                    response = await _client.PostAsync(uri, content);
+                }
+                else
+                {
+                    response = await _client.PutAsync(uri, content);
+                }
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine(@"\tTodoItem successfully saved.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+        }
+
+        public async Task DeleteElements(string id)
+        {
+            var uri = new Uri(string.Format(Constants.TutoripEndPoint, id));
+
+            try
+            {
+                var response = await _client.DeleteAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine(@"\tTodoItem successfully deleted.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
         }
     }
 }
